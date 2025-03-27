acf9416d7ef507ac15ca392444297f0d071b935d
SourceM7 <mohamadaladlan@yahoo.com>
2025-03-27 21:26:19 +0300

    job modification

--- app/Jobs/ProcessPodcastUrl.php
+++ app/Jobs/ProcessPodcastUrl.php
@@ -10,9 +10,13 @@ use Illuminate\Foundation\Queue\Queueable;
 class ProcessPodcastUrl implements ShouldQueue
 {
     use Queueable;
+
     public $rssUrl;
+
     public $listeningParty;
+
     public $episode;
+
     /**
      * Create a new job instance.
      */
@@ -34,26 +38,57 @@ class ProcessPodcastUrl implements ShouldQueue
         $podcastArtworkUrl = $xml->channel->image->url;
 
         $latestEpisode = $xml->channel->item[0];
+
         $episodeTitle = $latestEpisode->title;
         $episodeMediaUrl = (string) $latestEpisode->enclosure['url'];
 
-        $namespaces=$xml->getNamespaces(true);
-        $itunesNamespace=$namespaces['itunes'];
+        $namespaces = $xml->getNamespaces(true);
+        $itunesNamespace = $namespaces['itunes'] ?? null;
 
-        $episodeLength = $latestEpisode->children($itunesNamespace)->duration;
+        $episodeLength = null;
 
-        $interval = CarbonInterval::createFromFormat('H:i:s',$episodeLength);
+        if ($itunesNamespace) {
+            $episodeLength = (string) $latestEpisode->children($itunesNamespace)->duration;
+        }
 
-        $endTime= $this->listeningParty->start_time->add($interval);
+        if (empty($episodeLength)) {
+            $fileSize = (int) $latestEpisode->enclosure['length'];
+            $bitrate = 128000;
+            $durationInSeconds = ceil($fileSize * 8 / $bitrate);
+            $episodeLength = (string) $durationInSeconds;
+        }
+
+        
+        try {
+            if (strpos($episodeLength, ':') !== false) {
+                
+                $parts = explode(':', $episodeLength);
+                if (count($parts) == 2) {
+                    $interval = CarbonInterval::createFromFormat('i:s', $episodeLength);
+                } elseif (count($parts) == 3) {
+                    $interval = CarbonInterval::createFromFormat('H:i:s', $episodeLength);
+                } else {
+                    throw new \Exception('Unexpected duration format');
+                }
+            } else {
+                $interval = CarbonInterval::seconds((int) $episodeLength);
+            }
+        } catch (\Exception $e) {
+            Log::error('Error parsing episode duration: '.$e->getMessage());
+            $interval = CarbonInterval::hour();
+        }
+
+        $endTime = $this->listeningParty->start_time->add($interval);
 
 
-        $podcast=Podcast::updateOrCreate([
+        $podcast = Podcast::updateOrCreate([
             'title' => $podcastTitle,
             'artwork_url' => $podcastArtworkUrl,
-            'rss_url'=>$this->rssUrl
+            'rss_url' => $this->rssUrl,
         ]);
 
         $this->episode->podcast()->associate($podcast);
+
         $this->episode->update([
             'title' => $episodeTitle,
             'media_url' => $episodeMediaUrl,
@@ -62,5 +97,6 @@ class ProcessPodcastUrl implements ShouldQueue
         $this->listeningParty->update([
             'end_time' => $endTime,
         ]);
+
     }
 }
